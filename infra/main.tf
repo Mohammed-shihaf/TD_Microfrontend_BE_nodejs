d8feda07858900bad498994d4017a8193da24943
Mohammed-shihaf <shihaf@Testable.Cloud>
2026-08-13 18:46:27 +0530

    microfrontend_nodejs22: add shell script, IaC (Dockerfile+k8s+Terraform), and SOC2 doc for readiness scanner

---
+++ infra/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_version = ">= 1.7.0"
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "app" {
+  name = "td-microfrontend-be-nodejs:latest"
+  build {
+    context    = "."
+    dockerfile = "infra/Dockerfile"
+  }
+}
+
+resource "docker_container" "app" {
+  name  = "td-microfrontend-be-nodejs-container"
+  image = docker_image.app.image_id
+  ports {
+    internal = 3000
+    external = 3000
+  }
+}
